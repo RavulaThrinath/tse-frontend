9e7607c588b87f2c12cb906bab26eb67c048a2c0
RavulaThrinath <thrinathravual@gmail.com>
2022-06-22 09:36:44 +0530

    responsive code

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -5,26 +5,26 @@ import "./About.css";
 export default function About() {
   return (
     <>
-      <div className="about-container">
-        <div className="
-        conatiner-wrapper">
-          <h2 className="container-heading">About TSE</h2>
-          <p className="container-text">
-            TSE LTD is a fintech trading consulting firm dedicated to making the
-            business of financial trading seamless for individual
-            traders/investors regardless of their knowledge and technical
-            capacities adopting a financial technology and innovative approach
-            to risk mitigation and portfolio management (RMPM) offering traders
-            a solution for seamless, profitable and financial growth.
-          </p>
-          <p className="container-text">
-            We believe in working closely with our clients and we’re willing to
-            adapt our services to respond to the rising demand for trading
-            solutions to traders/investors as financial trading in the global
-            market is gaining more populace. We will address any of your trading
-            concerns with an innovative, apt and dynamic approach.
-          </p>
-        </div>
+      <div className="services-heading about-img">
+        <h1 className="img-heading">About TSE</h1>
+      </div>
+      <div className="services-content">
+        <h2 className="content-title">Who we are</h2>
+        <p className="content-text who-we-are">
+          TSE LTD is a fintech trading consulting firm dedicated to making the
+          business of financial trading seamless for individual
+          traders/investors regardless of their knowledge and technical
+          capacities adopting a financial technology and innovative approach to
+          risk mitigation and portfolio management (RMPM) offering traders a
+          solution for seamless, profitable and financial growth.
+        </p>
+        <p className="content-text">
+          We believe in working closely with our clients and we’re willing to
+          adapt our services to respond to the rising demand for trading
+          solutions to traders/investors as financial trading in the global
+          market is gaining more populace. We will address any of your trading
+          concerns with an innovative, apt and dynamic approach.
+        </p>
       </div>
       <Teams />
     </>
